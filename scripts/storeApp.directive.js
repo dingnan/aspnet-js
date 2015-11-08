c71830ca37a960f8b09641f364d3e4c7b1f3ce4d
unknown <nandingb@gmail.com>
2015-11-08 18:12:19 -0500

    add directive

---
+++ scripts/storeApp.directive.js
@@ -0,0 +1,10 @@
+﻿angular.module('storeApp')
+.directive('product', function () {
+    return {
+        restrict: 'E',
+        template: '<div><h4>{{item.title}}</h4></div><div><img ng-src="{{item.imageUrl}}" /></div><div><h4>{{item.price|currency}}</h4></div>',
+        scope: {
+            item: '='
+        }
+    };
+});
